101e1f639b799a7b030e3f2cb810f59eed003140
Lawrence Hoerst <hoerstl@berea.edu>
2023-05-23 14:57:02 -0400

    Added automatic code generation

--- AtollBoard.py
+++ AtollBoard.py
@@ -3,79 +3,135 @@ import random
 
 # https://py2.codeskulptor.org/#user50_bK7Eg8ebJv_1.py
 
-def randompartition(n, k):
-    result = [1 for i in range(k)]
-    choose = n - k
-    j = 0
-    while choose != 0 and j < k:
-        p = random.choice(range(choose + 1))
-        choose -= p
-        result[j] += p
-        j += 1
-    result[k - 1] += choose
-    return result
+
+class AtollBoard:
+    def __init__(self, size):
+        n = size
+        board = [i for i in range(n)]
+        self.board = []
+
+        remainingSpaces = board[:]
+        if len(remainingSpaces) == 2:
+            p1SpaceCount = 1
+        else:
+            p1SpaceCount = random.choice(range(len(remainingSpaces) - 2)) + 1
+        p1Spaces = random.sample(remainingSpaces, p1SpaceCount)
+        p1Distribution = self.randompartition(n, p1SpaceCount)
+        for i in range(len(p1Spaces)):
+            self.board.append((p1Spaces[i], p1Distribution[i], "L"))
+            remainingSpaces.remove(p1Spaces[i])
+
+        if len(remainingSpaces) == 1:
+            p2SpaceCount = 1
+        else:
+            p2SpaceCount = random.choice(range(len(remainingSpaces) - 1)) + 1
+        p2Spaces = random.sample(remainingSpaces, p2SpaceCount)
+        p2Distribution = self.randompartition(n, p2SpaceCount)
+        for i in range(len(p2Spaces)):
+            self.board.append((p2Spaces[i], p2Distribution[i], "R"))
+            remainingSpaces.remove(p2Spaces[i])
+
+        for i in range(len(remainingSpaces)):
+            self.board.append((remainingSpaces[i], 0, "N/A"))
+
+        self.board.sort(key=self.getfirstindex)
+        for i in range(len(self.board)):
+            self.board[i] = self.board[i][1:]
 
 
-def getfirstindex(object):
-    return object[0]
+
+        # print(self.board)
+        if p1SpaceCount > p2SpaceCount:
+            # print("Right Starts")
+            self.startingPlayer = "R"
+        elif p1SpaceCount == p2SpaceCount:
+            # print("Left Starts")
+            self.startingPlayer = "L"
+        elif p1SpaceCount < p2SpaceCount:
+            # print("Left Starts")
+            self.startingPlayer = "L"
+        else:
+            raise RuntimeError("Somehow there are more than 3 comparisons between two fixed integers!")
+
+    def randompartition(self, n, k):
+        result = [1 for i in range(k)]
+        choose = n - k
+        j = 0
+        while choose != 0 and j < k:
+            p = random.choice(range(choose + 1))
+            choose -= p
+            result[j] += p
+            j += 1
+        result[k - 1] += choose
+        return result
 
 
-def createBoard(size):
-    n = size
-    board = [i for i in range(n)]
-    finalBoard = []
+    def generateCode(self, playerPerspective):
+        code = ""
+        for space_ in range(len(self.board)):
+            lspace = self.getSpace(space_, -1)
+            thisSpace = self.getSpace(space_, 0)
+            rspace = self.getSpace(space_, 1)
 
-    remainingSpaces = board[:]
-    if len(remainingSpaces) == 2:
-        p1SpaceCount = 1
-    else:
-        p1SpaceCount = random.choice(range(len(remainingSpaces) - 2)) + 1
-    p1Spaces = random.sample(remainingSpaces, p1SpaceCount)
-    p1Distribution = randompartition(n, p1SpaceCount)
-    for i in range(len(p1Spaces)):
-        finalBoard.append((p1Spaces[i], p1Distribution[i], "L"))
-        remainingSpaces.remove(p1Spaces[i])
+            if thisSpace[1] == "N/A":
+                continue
 
-    if len(remainingSpaces) == 1:
-        p2SpaceCount = 1
-    else:
-        p2SpaceCount = random.choice(range(len(remainingSpaces) - 1)) + 1
-    p2Spaces = random.sample(remainingSpaces, p2SpaceCount)
-    p2Distribution = randompartition(n, p2SpaceCount)
-    for i in range(len(p2Spaces)):
-        finalBoard.append((p2Spaces[i], p2Distribution[i], "R"))
-        remainingSpaces.remove(p2Spaces[i])
+            # Generate the space's current threat level
+            aggressionScore = 0
+            if lspace[1] != thisSpace[1]:
+                aggressionScore += lspace[0]
+            if rspace[1] != thisSpace[1] and rspace is not lspace:
+                aggressionScore += rspace[0]
+            # See if the space can be aggressed upon
+            if aggressionScore > thisSpace[0]:
+                if playerPerspective == thisSpace[1]:
+                    code += "D"
+                else:
+                    code += "T"
+            else:
+                if playerPerspective == thisSpace[1]:
+                    code += "S"
+                else:
+                    code += "P"
 
-    for i in range(len(remainingSpaces)):
-        finalBoard.append((remainingSpaces[i], 0, "N/A"))
 
-    finalBoard.sort(key=getfirstindex)
-    for i in range(len(finalBoard)):
-        finalBoard[i] = finalBoard[i][1:]
+            # Check its relationship to the right neighbor
+            if rspace[1] == "N/A" or rspace[1] == thisSpace[1]:
+                code += "|"
 
-    #print(finalBoard)
-    if p1SpaceCount > p2SpaceCount:
-        #print("Right Starts")
-        startingPlayer = "R"
-    elif p1SpaceCount == p2SpaceCount:
-        #print("Left Starts")
-        startingPlayer = "L"
-    elif p1SpaceCount < p2SpaceCount:
-        #print("Left Starts")
-        startingPlayer = "L"
-    else:
-        raise RuntimeError("Somehow there are more than 3 comparisons between two fixed integers!")
+            elif thisSpace[1] != rspace[1]:
+                if thisSpace[1] == playerPerspective:
+                    if thisSpace[0] > rspace[0]:
+                        code += "⬆"
+                    elif thisSpace[0] < rspace[0]:
+                        code += "⬇"
+                    else:
+                        code += "="
+                else:
+                    if rspace[0] > thisSpace[0]:
+                        code += "⬆"
+                    elif rspace[0] < thisSpace[0]:
+                        code += "⬇"
+                    else:
+                        code += "="
 
-    return startingPlayer, finalBoard
+        return code
+
+
+
+    def getSpace(self, start, offset):
+        return self.board[(start + offset) % len(self.board)]
+
+
+    def getfirstindex(self, obj):
+        return obj[0]
+
+
+
 
 
 if __name__ == '__main__':
     n = int(input("Please enter the size of the atoll: "))
-    for i in range(10):
-        startingPlayer, board = createBoard(n)
-
-        print(startingPlayer + " Starts")
-        print(board)
 
 
 
--- drawAtollBoard.py
+++ drawAtollBoard.py
@@ -1,5 +1,5 @@
 import turtle
-import AtollBoard
+from AtollBoard import *
 
 
 
@@ -19,9 +19,11 @@ def drawNewBoard():
     if islandNumber == 0:
         return
     #print(islandNumber)
-    startingPlayer, board = AtollBoard.createBoard(islandNumber)
+    newboard = AtollBoard(islandNumber)
+    code = newboard.generateCode(newboard.startingPlayer)
+
     if not releaseMode:
-        print(board)
+        print(newboard.board)
 
     t.reset()
     t.speed(0)
@@ -54,25 +56,27 @@ def drawNewBoard():
     t.goto(0, rmid - 20)
     t.circle(-rmid, extent=rotationDelta/2)
     for i in range(islandNumber):
-        if board[i][1] == "L":
+        if newboard.board[i][1] == "L":
             t.color("blue")
-        elif board[i][1] == "R":
+        elif newboard.board[i][1] == "R":
             t.color("red")
         else:
             t.color("white")
-        t.write(board[i][0], align="center", font=('Arial', 30, 'normal'))
+        t.write(newboard.board[i][0], align="center", font=('Arial', 30, 'normal'))
         t.circle(-rmid, extent=rotationDelta)
 
 
     t.penup()
     t.goto(500, -100)
-    if startingPlayer == "L":
+    if newboard.startingPlayer == "L":
         t.color("blue")
-    elif startingPlayer == "R":
+    elif newboard.startingPlayer == "R":
         t.color("red")
     else:
         raise RuntimeError("Weird starting Player")
-    t.write(startingPlayer + " to Start", align="center", font=('Arial', 40, 'normal'))
+    t.write(newboard.startingPlayer + " to Start", align="center", font=('Arial', 40, 'normal'))
+    t.goto(0, 0)
+    t.write(code, align="center", font=('Arial', 20, 'normal'))
 
 
 
@@ -87,9 +91,10 @@ if __name__ == '__main__':
     releaseMode = True
     root = turtle.Screen()
     t = turtle.Turtle()
+    if releaseMode:
+        t.hideturtle()
     t.speed(0)
-    islandNumber = 0
-    getNewSize()
+    islandNumber = 2
     root.onkeypress(drawNewBoard, "space")
     root.onkeypress(getNewSize, "s")
     root.listen()
