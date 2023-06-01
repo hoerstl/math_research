06e1451fb9c54c217fb2f926c6f408995cbaf0fe
NelsonXunic <89146860+NelsonXunic@users.noreply.github.com>
2023-06-01 12:04:47 -0400

    added memoization

--- Meagan's Tripartite Graphs.py
+++ Meagan's Tripartite Graphs.py
@@ -63,12 +63,12 @@ def getNimValue(original):
         return nimValue
 
     # TODO: Remove this if statement after one run
-    if reduced.size == 0:
-        graphs[graphKey] = 0
-        return 0
+    # if reduced.size == 0:
+    #     graphs[graphKey] = 0
+    #     return 0
 
     childGraphs = []
-    childNimValues = []
+    childNimValues = set()
 
     for vertex in getVertexMoves(reduced):
         childGraphs.append(removeVertex(reduced, vertex))
@@ -77,8 +77,9 @@ def getNimValue(original):
         childGraphs.append(removeEdge(reduced, edgeMove))
 
     for graph in childGraphs:
-        childNimValues.append(getNimValue(graph))
+        childNimValues.add(getNimValue(graph))
 
+    childNimValues = list(childNimValues)
     childNimValues.sort()
 
     for i in range(len(childNimValues)):
@@ -90,58 +91,56 @@ def getNimValue(original):
     graphs[graphKey] = len(childNimValues)
     return len(childNimValues)
 
+#
+# size = 7
+# graph1 = np.zeros((size, size), dtype=int)
+# edges1 = [(0, 1), (1, 2), (1, 4), (2, 3), (4, 3), (3, 5), (5, 6)]
+# graph1 = attachEdges(graph1, edges1)
+#
+# graph1_reduced = reduce(graph1).tolist()
+#
+# print("input graph: ")
+# print(graph1)
+#
+# print("reduced graph: ")
+# print(graph1_reduced)
+#
+#
+# graph2 = np.zeros((size, size), dtype=int)
+# edges2 = [(2, 4), (4, 5), (4, 6), (5, 0), (6, 0), (0, 1), (1, 3)]
+# graph2 = attachEdges(graph2, edges2)
+#
+# # graph2_reduced = str(reduce(graph2))
+# # graph2_reduced = reduce(graph2)
+#
+# # 2023-06-01: Testing np.delete()
+# # graph2_chomp = np.delete(graph2_reduced, 6, 1)
+# # print(f"CHOMPED GRAPH2 {graph2_chomp}")
+#
+#
+# print("input graph2: ")
+# print(graph2)
+#
+# print("reduced graph2: ")
+# print(graph2_reduced)
+#
+# # print(getVertexMoves(graph2_reduced))
+# # print(getEdgeMoves(graph2_reduced))
+#
+# # print(f"Removed vertex 2 {removeVertex(graph2_reduced, 2)}")
+# # print(f"Removed edge (1,4) {removeEdge(graph2_reduced, (1,4))}")
+#
+#
+# graph2_reduced = str(graph2_reduced)
+#
+# if len(edges1) != len(edges2):
+#     print("There was a keying error")
+#
+# if graph1_reduced == graph2_reduced:
+#     print("The graphs are isomorphic!")
+# else:
+#     print("These graphs are not isomorphic.")
 
-
-size = 7
-graph1 = np.zeros((size, size), dtype=int)
-edges1 = [(0, 1), (1, 2), (1, 4), (2, 3), (4, 3), (3, 5), (5, 6)]
-graph1 = attachEdges(graph1, edges1)
-
-graph1_reduced = reduce(graph1).tolist()
-
-print("input graph: ")
-print(graph1)
-
-print("reduced graph: ")
-print(graph1_reduced)
-
-
-graph2 = np.zeros((size, size), dtype=int)
-edges2 = [(2, 4), (4, 5), (4, 6), (5, 0), (6, 0), (0, 1), (1, 3)]
-graph2 = attachEdges(graph2, edges2)
-
-# graph2_reduced = str(reduce(graph2))
-graph2_reduced = reduce(graph2)
-
-# 2023-06-01: Testing np.delete()
-# graph2_chomp = np.delete(graph2_reduced, 6, 1)
-# print(f"CHOMPED GRAPH2 {graph2_chomp}")
-
-
-print("input graph2: ")
-print(graph2)
-
-print("reduced graph2: ")
-print(graph2_reduced)
-
-# print(getVertexMoves(graph2_reduced))
-# print(getEdgeMoves(graph2_reduced))
-
-# print(f"Removed vertex 2 {removeVertex(graph2_reduced, 2)}")
-# print(f"Removed edge (1,4) {removeEdge(graph2_reduced, (1,4))}")
-
-
-graph2_reduced = str(graph2_reduced)
-
-if len(edges1) != len(edges2):
-    print("There was a keying error")
-
-if graph1_reduced == graph2_reduced:
-    print("The graphs are isomorphic!")
-else:
-    print("These graphs are not isomorphic.")
-
-graphs = {graph2_reduced: 10}
 
 if __name__ == '__main__':
     size = int(input("How many vertices does the graph have: "))
@@ -153,29 +152,20 @@ if __name__ == '__main__':
     for edge_number in range(num_edges):
         first_num = int(edges[4*edge_number])
         second_num = int(edges[4*edge_number+2])
-
         ordered_edges.append((first_num, second_num))
 
     graph = np.zeros((size, size), dtype=int)
     graph = attachEdges(graph, ordered_edges)
-    graph = str(reduce(graph))
 
-    if graphs.get(graph, False):
-        print(graphs[graph])
-    else:
-        print("Sorry bro!")
+    # graphs = {}
+    with open("graphs.dict", "rb") as file:
+        graphs = pickle.load(file)
 
-    print(graph)
-
-    # vertex = int(input("What vertex to we remove: "))
-    # edge = input("What edge do we remove: ")
-
-
+    print(getNimValue(graph))
 
     # graph2 : 7  2,4,4,5,4,6,5,0,6,0,0,1,1,3
 
-
-
-
+    with open("graphs.dict", "wb") as file:
+        pickle.dump(graphs, file)
 
 
