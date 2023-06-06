3ab9dbc3537fad1478a5a67c9b22214ce8d7f6d6
Lawrence Hoerst <hoerstl@berea.edu>
2023-06-06 09:22:30 -0400

    Easy entry is now available for all n-partite graphs

--- Meagans_Tripartite_Graphs.py
+++ Meagans_Tripartite_Graphs.py
@@ -4,26 +4,41 @@ import numpy as np
 import oapackage
 import pickle
 
+# AHOY!!!!
+# Meagan, you can change this to a number instead of having to type it in each time.
+# Change it back to "0" without the quotes to have it ask each time.
+MANUAL_PARTITE = 0
 
-def getTripartiteEdges(vertexCount, firstCount, secondCount, thirdCount):
-    if firstCount + secondCount + thirdCount != vertexCount:
-        print(f"Unfortunately, {firstCount} + {secondCount} + {thirdCount} is not equal to {vertexCount}. Try again! ○|￣|_")
-        raise RuntimeError("Inconsistent number of verticies given to tripartite edges.")
 
-    first = list(range(firstCount))
-    second = list(range(firstCount, firstCount + secondCount))
-    third = list(range(firstCount + secondCount, firstCount + secondCount + thirdCount))
+def getTripartiteEdges(vertexCount, partitionsList):
+    sums = []
+    for i, partitionValue in enumerate(partitionsList):
+        if i == 0:
+            sums.append(partitionValue)
+        else:
+            sums.append(sums[-1] + partitionValue)
+
+    if sums[-1] != vertexCount:
+        return f"Unfortunately, your values sum to be {sums[-1]} which is not equal to {vertexCount}. Try again! ○|￣|_"
+
+    partitions = []
+    for i in range(len(partitionsList)):
+        if i > 0:
+            partitions.append(range(sums[i - 1], sums[i]))
+        else:
+            partitions.append(range(sums[i]))
+
+    # first = list(range(firstCount))
+    # second = list(range(firstCount, firstCount + secondCount))
+    # third = list(range(firstCount + secondCount, firstCount + secondCount + thirdCount))
     edges = []
     # Connect first verticies to all in the second and third partitions
-    for startVertex in first:
-        for endVertex in second:
-            edges.append((startVertex, endVertex))
-        for endVertex in third:
-            edges.append((startVertex, endVertex))
-    # Connect second vertices to all in the third partition
-    for startVertex in second:
-        for endVertex in third:
-            edges.append((startVertex, endVertex))
+    for startPartitionNum, startPartition in enumerate(partitions):
+        for startVertex in startPartition:
+            for endPartitionNum in range(startPartitionNum + 1, len(partitions)):
+                for endVertex in partitions[endPartitionNum]:
+                    edges.append((startVertex, endVertex))
+
 
     return edges
 
@@ -85,8 +100,6 @@ def getNimValue(original):
     if nimValue is not None:
         return nimValue
 
-
-
     childGraphs = []
 
     for vertex in getVertexMoves(reduced):
@@ -112,92 +125,64 @@ def getNimValue(original):
     graphs[graphKey] = len(childNimValues)
     return len(childNimValues)
 
-"""
-size = 7
-graph1 = np.zeros((size, size), dtype=int)
-edges1 = [(0, 1), (1, 2), (1, 4), (2, 3), (4, 3), (3, 5), (5, 6)]
-graph1 = attachEdges(graph1, edges1)
-
-graph1_reduced = reduce(graph1).tolist()
-
-print("input graph: ")
-print(graph1)
-
-print("reduced graph: ")
-print(graph1_reduced)
-
-
-graph2 = np.zeros((size, size), dtype=int)
-edges2 = [(2, 4), (4, 5), (4, 6), (5, 0), (6, 0), (0, 1), (1, 3)]
-graph2 = attachEdges(graph2, edges2)
-
-# graph2_reduced = str(reduce(graph2))
-# graph2_reduced = reduce(graph2)
-
-# 2023-06-01: Testing np.delete()
-# graph2_chomp = np.delete(graph2_reduced, 6, 1)
-# print(f"CHOMPED GRAPH2 {graph2_chomp}")
-
-
-print("input graph2: ")
-print(graph2)
-
-print("reduced graph2: ")
-print(graph2_reduced)
-
-# print(getVertexMoves(graph2_reduced))
-# print(getEdgeMoves(graph2_reduced))
-
-# print(f"Removed vertex 2 {removeVertex(graph2_reduced, 2)}")
-# print(f"Removed edge (1,4) {removeEdge(graph2_reduced, (1,4))}")
-
-
-graph2_reduced = str(graph2_reduced)
-
-if len(edges1) != len(edges2):
-    print("There was a keying error")
-
-if graph1_reduced == graph2_reduced:
-    print("The graphs are isomorphic!")
-else:
-    print("These graphs are not isomorphic.")
-"""
 
 def main():
-    global graphs
+    global graphs, MANUAL_PARTITE
     while True:
         size = int(input("How many vertices does the graph have: "))
         if size < 0:
             quit()
         edges = input("What are the edges? (e.x. 1,2;3,4;5,6; or 'complete'): ")
+        manualEntry = True
+        inputError = None
+        if edges.lower()[0] == "c":
+            print("Ahhh, I see you're looking for a complete graph (ง •_•)ง")
+            manualEntry = False
+            numpartitions = MANUAL_PARTITE
+            while numpartitions < 1 or numpartitions > size:
+                numpartitions = int(input(f"How many partitions would you like to split your {size} vertices into: "))
+            partitions = [0 for i in range(numpartitions)]
+            extraNodes = size - numpartitions
+            print(f"Currently generating a graph with {numpartitions} partitions...")
+            for i in range(numpartitions):
+                if i+1 % 10 == 1:
+                    postfix = "st"
+                elif i+1 % 10 == 2:
+                    postfix = "nd"
+                elif i+1 % 10 == 3:
+                    postfix = "rd"
+                else:
+                    postfix = "th"
+                response = -1
+                while response < 1 or response - 1 > extraNodes:
+                    response = int(input(f"How many verticies in the {i+1}{postfix} position: "))
+                extraNodes -= response - 1
+                partitions[i] = response
+            ordered_edges = getTripartiteEdges(size, partitions)
+            if isinstance(ordered_edges, str):
+                inputError = ordered_edges
 
-        if edges == "complete":
-            print("Ahhh, I see you're looking for a complete tripartite graph (ง •_•)ง")
-            first = int(input("How many verticies in the first partition: "))
-            second = int(input("How many verticies in the second partition: "))
-            third = int(input("How many verticies in the third partition: "))
-            ordered_edges = getTripartiteEdges(size, first, second, third)
+
+        if manualEntry:
+            edgeExpression = r"[0-9]+"
+            ordered_edges = []
+            integers = re.findall(edgeExpression, edges)
+
+
+            if len(integers) % 2 == 1:
+                inputError = f"User entered {len(integers)} ends that the edges connect to. This cannot be odd."
+            for edge_Number in range(len(integers)//2):
+                first_num = int(integers[2 * edge_Number])
+                second_num = int(integers[2 * edge_Number + 1])
+                ordered_edges.append((first_num, second_num))
+                if first_num >= size:
+                    inputError = f"User error, {first_num} is not in the acceptable range of labels from 0 to {size - 1}. Try again!"
+                if second_num >= size:
+                    inputError = f"User error, {second_num} is not in the acceptable range of labels from 0 to {size - 1}. Try again!"
+
+        if inputError is None:
             break
-
-        edgeExpression = r"[0-9]+"
-        ordered_edges = []
-        integers = re.findall(edgeExpression, edges)
-        edgeError = None
-
-        if len(integers) % 2 == 1:
-            edgeError = f"User entered {len(integers)} ends that the edges connect to. This cannot be odd."
-        for edge_Number in range(len(integers)//2):
-            first_num = int(integers[2 * edge_Number])
-            second_num = int(integers[2 * edge_Number + 1])
-            ordered_edges.append((first_num, second_num))
-            if first_num >= size:
-                edgeError = f"User error, {first_num} is not in the acceptable range of labels from 0 to {size - 1}. Try again!"
-            if second_num >= size:
-                edgeError = f"User error, {second_num} is not in the acceptable range of labels from 0 to {size - 1}. Try again!"
-
-        if edgeError is None:
-            break
-        print(edgeError)
+        print(inputError)
 
     graph = np.zeros((size, size), dtype=int)
     graph = attachEdges(graph, ordered_edges)
@@ -211,10 +196,9 @@ def main():
 
     print(f"The nim value of \n{graph} \nis {getNimValue(graph)}")
 
-    # graph2 : 7  2,4,4,5,4,6,5,0,6,0,0,1,1,3
-
     with open("graphs.dict", "wb") as file:
         pickle.dump(graphs, file)
+
 
 if __name__ == '__main__':
     graphs = None
